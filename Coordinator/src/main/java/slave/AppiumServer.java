1b79b35a94d1f5f7858fc944c56ae4ff5bfca115
dieseldeesh <adhishramkumar@yahoo.com>
2015-11-30 13:54:59 -0500

    adding slave registration with primary

--- Coordinator/src/main/java/slave/AppiumServer.java
+++ Coordinator/src/main/java/slave/AppiumServer.java
@@ -1,5 +1,7 @@
 package slave;
 
+import java.io.IOException;
+import java.net.DatagramSocket;
 import java.net.SocketException;
 import java.util.NoSuchElementException;
 import java.util.Queue;
@@ -7,11 +9,21 @@ import java.util.UUID;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 import message.Message;
+import message.MessageTypes;
+import message.msgconstructor.WhoIsPrimaryConstructor;
+
+import org.json.JSONObject;
+
+import services.common.NetServiceFactory;
+import services.common.NetServiceProxy;
 import services.io.NetConfig;
+import shared.ConnMetrics;
 import shared.Job;
 import distributor.Distributer;
+import error.WrongMessageTypeException;
 
 public class AppiumServer extends Distributer {
+	private static final int QUERY_PORT = 12345;
 	private Queue<Job> taskQueue;
 	private ConnectionHandler connectionHandler;
 	private TaskExecutor taskExecutor;
@@ -26,8 +38,33 @@ public class AppiumServer extends Distributer {
 
 	@Override
 	public void serve() {
-		this.connectionHandler.run();
-		this.taskExecutor.run();
+		try {
+			getPrimary();
+			this.connectionHandler.run();
+			this.taskExecutor.run();
+		} catch (IOException | WrongMessageTypeException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void getPrimary() throws IOException, WrongMessageTypeException {
+		DatagramSocket socket = new DatagramSocket(QUERY_PORT);
+		Message msg = WhoIsPrimaryConstructor.constructQuery(this.ip,
+				QUERY_PORT);
+		NetServiceProxy proxy = NetServiceFactory.getRawUDPService();
+		proxy.sendMessage(msg, socket, new NetConfig(
+				ConnMetrics.IPOfCoordinator,
+				ConnMetrics.portOfCoordinatorForPrimaryAddr));
+		Message response = proxy.receiveMessage(socket);
+		socket.close();
+		if (response.getType() == MessageTypes.WHOISPRIMARY) {
+			JSONObject obj = new JSONObject(response.getContent());
+			this.coordinator = new NetConfig(obj.getString("ip"),
+					ConnMetrics.portReceiveHeartBeatFromSlave);
+		} else {
+			throw new WrongMessageTypeException(response.getType(),
+					MessageTypes.WHOISPRIMARY);
+		}
 	}
 
 	public void closeConnections() {
