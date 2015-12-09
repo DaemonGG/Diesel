b05ad93b568b243b21939e00b25008ab3feaf0f4
DaemonGG <xchjin@hotmail.com>
2015-12-09 00:53:54 -0500

    color

--- Coordinator/src/main/java/shared/CurrentTime.java
+++ Coordinator/src/main/java/shared/CurrentTime.java
@@ -8,6 +8,12 @@ import java.util.TimeZone;
  * Created by xingchij on 12/1/15.
  */
 public class CurrentTime {
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+
     public static String getTime(){
         SimpleDateFormat pattern = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
         pattern.setTimeZone(TimeZone.getDefault());
@@ -15,6 +21,6 @@ public class CurrentTime {
     }
 
     public static void tprintln(String s){
-        System.out.println(getTime() + '\t' + s);
+        System.out.println( ANSI_CYAN + getTime() + '\t' + ANSI_RESET +s);
     }
 }
